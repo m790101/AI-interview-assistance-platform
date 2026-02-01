aac73d70d18a563d00eb6d52cbc37a69c84534d9
haowen.cheng <haowensax@gmail.com>
2026-02-02 00:03:58 +0800

    chore: remove unused import

--- src/main/java/interview/guide/common/config/S3Config.java
+++ src/main/java/interview/guide/common/config/S3Config.java
@@ -1,7 +1,6 @@
 package interview.guide.common.config;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
